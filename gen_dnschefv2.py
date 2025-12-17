1ca6943d65aac7e6e4699ab88fe0666c821a5e76
Chase Hatch <chase.hatch.business@gmail.com>
2025-12-16 21:17:04 -0800

    Refactor DNS records generation and cleanup
    Small adjustments found needed when actually used in a multi-DC environment

--- gen_dnschefv2.py
+++ gen_dnschefv2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import os, sys, argparse
+import os, sys, argparse, ipaddress
 from IPython import embed
 
 def getargs():
@@ -33,6 +33,7 @@ def main():
     ('\n'+(' '*indent)).join
 
     a_recs = fmt(indent,[f"{x['dc']}={x['ip']}" for x in processed_items])
+    aaaa_recs = fmt(indent,[f"{x['dc']}={ipaddress.IPv6Address('::ffff:' + x['ip']).exploded}" for x in processed_items])
     cname_recs = fmt(indent,[f"{x['ip']}={x['dc']}" for x in processed_items])
     ptr_recs = fmt(indent,[f"{x['pi']}.in-addr-arpa={x['dc']}" for x in processed_items])
 
@@ -40,28 +41,25 @@ def main():
     kmastertcp = fmt(indent,[f"_kerberos-master._tcp.{x['domain']}=1 100 88 {x['dc']}" for x in processed_items])
     kerberos = fmt(indent,[f"_kerberos.{x['domain']}=1 100 88 {x['dc']}" for x in processed_items])
     ktcpdcmsdcs = fmt(indent,[f"_kerberos._tcp.dc._msdcs.{x['domain']}=1 100 88 {x['dc']}" for x in processed_items])
-    ktcpdcmsdcs2 = fmt(indent,[f"_kerberos._tcp.dc._msdcs=1 100 88 {x['dc']}" for x in processed_items])
 
     ldaptcp = fmt(indent,[f"_ldap._tcp.{x['domain']}=1 100 389 {x['dc']}" for x in processed_items])
     ldaptcpdcmsdcs = fmt(indent,[f"_ldap._tcp.dc._msdcs.{x['domain']}=1 100 389 {x['dc']}" for x in processed_items])
     ldaptcppdcmsdcs = fmt(indent,[f"_ldap._tcp.pdc._msdcs.{x['domain']}=1 100 389 {x['dc']}" for x in processed_items])
-    ldaptcppdcmsdcs2 = fmt(indent,[f"_ldap._tcp.pdc._msdcs=1 100 389 {x['dc']}" for x in processed_items])
     ldaptcpgcmsdcs = fmt(indent,[f"_ldap._tcp.gc._msdcs.{x['domain']}=1 100 3268 {x['dc']}" for x in processed_items])
-    ldaptcpgcmsdcs2 = fmt(indent,[f"_ldap._tcp.gc._msdcs=1 100 3268 {x['dc']}" for x in processed_items])
 
     ldapstcp = fmt(indent,[f"_ldaps._tcp.{x['domain']}=1 100 636 {x['dc']}" for x in processed_items])
     ldapstcpdcmsdcs = fmt(indent,[f"_ldaps._tcp.dc._msdcs.{x['domain']}=1 100 636 {x['dc']}" for x in processed_items])
     ldapstcppdcmsdcs = fmt(indent,[f"_ldaps._tcp.pdc._msdcs.{x['domain']}=1 100 636 {x['dc']}" for x in processed_items])
-    ldapstcppdcmsdcs2 = fmt(indent,[f"_ldaps._tcp.pdc._msdcs=1 100 636 {x['dc']}" for x in processed_items])
     ldapstcpgcmsdcs = fmt(indent,[f"_ldaps._tcp.gc._msdcs.{x['domain']}=1 100 3269 {x['dc']}" for x in processed_items])
-    ldapstcpgcmsdcs2 = fmt(indent,[f"_ldaps._tcp.gc._msdcs=1 100 3269 {x['dc']}" for x in processed_items])
-
     bloodhounds = fmt(indent,[f"netexec ldap {x['dc']} -k --use-kcache --bloodhound -c all --dns-server 127.0.0.1 [,--dns-tcp]" for x in processed_items])
     bloodhoundCEs = fmt(indent,[f"bloodhound-ce-python -u 'Administrator@{x['domain'].lower()}' -p 'sumptin123!' -d {x['domain'].lower()} -dc {x['dc']} -ns 127.0.0.1 [,--dns-tcp]" for x in processed_items])
     
     output = '\n'.join([x[indent:] for x in f""" 
         [A]
         {a_recs}
+
+        [AAAA]
+        {aaaa_recs}
 
         [CNAME]
         {cname_recs}
@@ -74,24 +72,19 @@ def main():
         {kmastertcp}
         {kerberos}
         {ktcpdcmsdcs}
-        {ktcpdcmsdcs2}
         {ldaptcp}
         {ldaptcpdcmsdcs}
         {ldaptcppdcmsdcs}
-        {ldaptcppdcmsdcs2}
         {ldaptcpgcmsdcs}
-        {ldaptcpgcmsdcs2}
         {ldapstcp}
         {ldapstcpdcmsdcs}
         {ldapstcppdcmsdcs}
-        {ldapstcppdcmsdcs2}
         {ldapstcpgcmsdcs}
-        {ldapstcpgcmsdcs2}
     """.split("\n")])
 
     output2 = '\n'.join([x[indent:] for x in f""" 
         ### Use one of the following, depending on DC hosts DNS on UDP53 or TCP53
-        dnschef -i 127.0.0.1 --nameservers {','.join([x['dc'] for x in processed_items])} --file cheffed.txt" [,-t]
+        dnschef -i 127.0.0.1 --nameservers {','.join([x['dc'] for x in processed_items])} --file cheffed.txt [,-t]
 
         Don't forget to update /etc/resolv.conf, add as FIRST nameserver:
             nameserver 127.0.0.1
@@ -109,12 +102,6 @@ def main():
         F.write(output)
         F.close()
     print(f"[+] dnschef config written to ./cheffed.txt")
-
-
-
-
-    
-
 
 
 if __name__=="__main__":
